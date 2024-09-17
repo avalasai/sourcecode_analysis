700bbbcb7e6fdb2c0b5b89f3df263abefb8d3802
avalasai <avalasai@gmail.com>
2024-09-17 11:41:55 -0700

    project outline updated and requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenerativeAI',
+    version = '0.0.1',
+    author='Sai Sampath Avala',
+    author_email='saisampath.avala@gmail.com',
+    packages = find_packages(),
+    install_requires = [],
+)
